c78953fd30d6036185ce75dbb1165872d52d91c3
Parker Mathewson <parkerfm34@gmail.com>
2013-09-25 22:19:39 -0700

    initialize setters and getters for song

--- src/model/Song.java
+++ src/model/Song.java
@@ -2,9 +2,44 @@ package model;
 
 public class Song {
 
-	private String name;
-	private String artist;
-	private int time;
+	private String songName;
+	private String artistName;
+	private int songTime;
 	private int dayCount;
 	private int fullCount;
+	
+	public void setSongName(String name)
+	{
+		this.songName = name;
+	}
+	
+	public void setArtistName(String name)
+	{
+		this.artistName = name;
+	}
+	
+	public void setSongTime(int time)
+	{
+		this.songTime = time;
+	}
+	
+	public void setDayCount(int count)
+	{
+		this.dayCount = count;
+	}
+	
+	public void setFullCount(int count)
+	{
+		this.fullCount = count;
+	}
+	
+	public String getSongName()
+	{
+		return this.songName;
+	}
+	
+	public String getArtistName()
+	{
+		return this.artistName;
+	}
 }
