3d4d3b225ae62983418c8b789c8ad5931acad02b
Parker Mathewson <parkerfm34@gmail.com>
2013-09-26 00:01:26 -0700

    added list to GUI and basic GUI things

--- src/model/SongList.java
+++ src/model/SongList.java
@@ -1,8 +1,97 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class SongList {
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+public class SongList implements TableModel {
 
 	private ArrayList<Song> songs = new ArrayList<Song>();
+	
+	public SongList()
+	{
+		setUpDefaultList();
+	}
+	
+	private void setUpDefaultList()
+	{
+		songs.add(new Song("Blue Ridge Mountain Mist", "Ralph Schuckett", 38));
+		songs.add(new Song("Determined Tumbao", "FreePlay Music", 20));
+		songs.add(new Song("Flute", "Sun Microsystems", 5));
+		songs.add(new Song("Space Music", "Unknown", 6));
+		songs.add(new Song("Swing Cheese", "FreePlay Music", 15));
+		songs.add(new Song("Tada", "Microsoft", 2));
+		songs.add(new Song("Untamable Fire", "Pierre Langer", 282));
+	}
+
+	@Override
+	public void addTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex)
+	{
+		if(columnIndex == 0 || columnIndex == 1)
+			return String.class;
+		else
+			return Integer.class;
+	}
+
+	@Override
+	public int getColumnCount()
+	{
+		return 3;
+	}
+
+	@Override
+	public String getColumnName(int columnIndex)
+	{
+		
+		if(columnIndex == 0)
+			return "Song Name";
+		else if(columnIndex == 1)
+			return "Artist";
+		else
+			return "Time";
+	}
+
+	@Override
+	public int getRowCount() {
+		return songs.size();
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		Song temp = songs.get(rowIndex);
+		if(columnIndex == 0)
+			return temp.getSongName();
+		else if(columnIndex == 1)
+			return temp.getArtistName();
+		else
+			return temp.getSongTime();
+		
+	}
+
+	@Override
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/view/JukeboxGUI.java
+++ src/view/JukeboxGUI.java
@@ -1,11 +1,60 @@
 package view;
 
-public class JukeboxGUI {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+import model.SongList;
+import model.StudentList;
+
+public class JukeboxGUI extends JFrame{
 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		new JukeboxGUI().setVisible(true);
+	}
+	
+	private TableModel modelOfSongs;
+	private JTable tableOfSongs;
+	
+	public JukeboxGUI()
+	{
+		String username = JOptionPane.showInputDialog("Please enter username");
+		
+		JOptionPane.showInputDialog("Please enter password for user " + username);
+		
+		if(true)
+		{
+			this.setVisible(true);
+			layoutGUI();
+		}
+	}
+	
+	private void layoutGUI()
+	{
+		setTitle("University of Arizona Student Jukebox");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+//		setBackground(Color.BLUE);
 
+		this.setSize(500, 500);
+		this.setLocation(200, 200);
+		
+		modelOfSongs = new SongList();
+		tableOfSongs = new JTable(modelOfSongs);
+		
+		JScrollPane scrollPane = new JScrollPane(tableOfSongs);
+		
+		add(scrollPane, BorderLayout.CENTER);
 	}
 
 }
