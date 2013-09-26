e4532a464622fba2591aa80d57f7379faec7c785
hopglascock <Hopglascock@gmail.com>
2013-09-26 15:18:42 -0700

    didnt change much

--- src/view/JukeboxGUI.java
+++ src/view/JukeboxGUI.java
@@ -19,72 +19,71 @@ import javax.swing.table.TableRowSorter;
 
 import model.SongList;
 
-public class JukeboxGUI extends JFrame{
-
+public class JukeboxGUI extends JFrame
+{
 
 	public static void main(String[] args)
 	{
 		String username = JOptionPane.showInputDialog("Please enter username");
-		
+
 		String password = JOptionPane.showInputDialog("Please enter password for user " + username);
-		
-		if(true)
-			new JukeboxGUI().setVisible(true);
+
+		if (true) new JukeboxGUI().setVisible(true);
 	}
-	
-	private TableModel modelOfSongs;
-	private JTable tableOfSongs;
-	
+
+	private TableModel songTableModel;
+	private JTable songTable;
+
 	public JukeboxGUI()
 	{
-			layoutGUI();
+		layoutGUI();
 	}
-	
+
 	private void layoutGUI()
 	{
 		setTitle("University of Arizona Student Jukebox");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-//		setBackground(Color.BLUE);
+
+		// setBackground(Color.BLUE);
 
 		this.setSize(700, 500);
 		this.setLocation(200, 200);
-		
-		modelOfSongs = new SongList();
-		tableOfSongs = new JTable(modelOfSongs);
-		tableOfSongs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
-		JScrollPane scrollPane = new JScrollPane(tableOfSongs);
-		
-		
-		RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(modelOfSongs);
-		
-		tableOfSongs.setRowSorter(sorter);
-		
+		songTableModel = new SongList();
+		songTable = new JTable(songTableModel);
+		songTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		JScrollPane scrollPane = new JScrollPane(songTable);
+
+		RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(songTableModel);
+
+		songTable.setRowSorter(sorter);
+
 		add(scrollPane, BorderLayout.CENTER);
-		
+
 		JButton songSelect = new JButton("Queue Song");
 		JPanel panel = new JPanel();
 		panel.add(songSelect);
 		add(panel, BorderLayout.WEST);
 		songSelect.addActionListener(new ButtonListener());
 	}
-	
+
 	private class ButtonListener implements ActionListener
 	{
 		@Override
 		public void actionPerformed(ActionEvent arg0)
 		{
-			int viewRow = tableOfSongs.getSelectedRow();
-			
+			int viewRow = songTable.getSelectedRow();
+
 			if (viewRow < 0)
-				System.out.println("index " + viewRow
-						+ " means no row is selected");
+				System.out.println("index " + viewRow + " means no row is selected");
 			else
 			{
-				int modelRow = tableOfSongs.convertRowIndexToModel(viewRow);
+				int modelRow = songTable.convertRowIndexToModel(viewRow);
 				System.out.println("index " + viewRow + " has the name '"
-						+ modelOfSongs.getValueAt(modelRow, 0) + "'");
+						+ songTableModel.getValueAt(modelRow, 0) + "'");
+//				 + songTable.getValueAt(songTable.getSelectedRow(),songTable.convertColumnIndexToModel(0))+"'");
+//				this is another way to so it
 			}
 		}
 	}
