ef85cfe42f5f8552efcf6ab5a8594d712d24ca71
hopglascock <Hopglascock@gmail.com>
2013-09-25 22:56:12 -0700

    changed student list and student added getters and setters

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,9 +1,74 @@
 package model;
 
-public class Student {
+public class Student
+{
 
 	private String name = "";
 	private int timeLeft;
 	private int dayCount;
+	private long ID;
 	private boolean activeStudent;
+	
+	public Boolean allowedToPlay()
+	{
+		//TODO IMPLEMENT METHOD	
+		
+		return true;
+	}
+	
+	public Boolean activeStudent(){
+		//TODO implement method 
+		return true;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getTimeLeft()
+	{
+		return timeLeft;
+	}
+
+	public void setTimeLeft(int timeLeft)
+	{
+		this.timeLeft = timeLeft;
+	}
+
+	public int getDayCount()
+	{
+		return dayCount;
+	}
+
+	public void setDayCount(int dayCount)
+	{
+		this.dayCount = dayCount;
+	}
+
+	public long getID()
+	{
+		return ID;
+	}
+
+	public void setID(long iD)
+	{
+		ID = iD;
+	}
+
+	public boolean isActiveStudent()
+	{
+		return activeStudent;
+	}
+
+	public void setActiveStudent(boolean activeStudent)
+	{
+		this.activeStudent = activeStudent;
+	}
+
 }
--- src/model/StudentList.java
+++ src/model/StudentList.java
@@ -2,8 +2,30 @@ package model;
 
 import java.util.ArrayList;
 
-public class StudentList {
+public class StudentList
+{
 
 	private ArrayList<Student> students = new ArrayList<Student>();
-	
+
+	public void addStudent(Student x)
+	{
+		//TODO IMPLEMENT METHOD
+	}
+
+	public Student removeStudent()
+	{
+		//TODO IMPLEMENT METHOD
+		return null;
+	}
+
+	public ArrayList<Student> getStudents()
+	{
+		return students;
+	}
+
+	public void setStudents(ArrayList<Student> students)
+	{
+		this.students = students;
+	}
+
 }
