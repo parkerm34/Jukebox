2046666d67c71b7d7c89955262b21faa61404916
Parker Mathewson <parkerfm34@gmail.com>
2013-09-25 21:52:15 -0700

    add some files

---
+++ src/model/Jukebox.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Jukebox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/QueueAndPlay5SongsInOrder.java
@@ -0,0 +1,13 @@
+package model;
+
+public class QueueAndPlay5SongsInOrder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
