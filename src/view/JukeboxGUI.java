c1a685ff7b4d6d774aaeecfc871f7b34f0f436d3
Parker Mathewson <parkerfm34@gmail.com>
2013-09-26 00:29:27 -0700

    add button selection and frame on the left

--- src/view/JukeboxGUI.java
+++ src/view/JukeboxGUI.java
@@ -10,7 +10,10 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.RowSorter;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import model.SongList;
 import model.StudentList;
@@ -22,7 +25,7 @@ public class JukeboxGUI extends JFrame{
 	{
 		String username = JOptionPane.showInputDialog("Please enter username");
 		
-		JOptionPane.showInputDialog("Please enter password for user " + username);
+		String password = JOptionPane.showInputDialog("Please enter password for user " + username);
 		
 		if(true)
 			new JukeboxGUI().setVisible(true);
@@ -43,15 +46,25 @@ public class JukeboxGUI extends JFrame{
 		
 //		setBackground(Color.BLUE);
 
-		this.setSize(500, 500);
+		this.setSize(700, 500);
 		this.setLocation(200, 200);
 		
 		modelOfSongs = new SongList();
 		tableOfSongs = new JTable(modelOfSongs);
+		tableOfSongs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		JScrollPane scrollPane = new JScrollPane(tableOfSongs);
 		
+		RowSorter<TableModel> sorter = new TableRowSorter<TableModel>(modelOfSongs);
+		
+		tableOfSongs.setRowSorter(sorter);
+		
 		add(scrollPane, BorderLayout.CENTER);
+		
+		JButton songSelect = new JButton("Queue Song");
+		JPanel panel = new JPanel();
+		panel.add(songSelect);
+		add(panel, BorderLayout.WEST);
 	}
 
 }
